f4379f5fcc6882d7ecac329c3face23f86204123
Alexandros Zachariades <alexandros@office.belugga.com>
2021-03-11 18:01:08 +0200

    changes for deployment

--- Model/SQContext.cs
+++ Model/SQContext.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SimplzQuestionnaire.Model
 {
-    // Add-Migration M3 -v
+    // Add-Migration M4 -v
     public class SQContext : DbContext
     {
         public SQContext(DbContextOptions<SQContext> options) : base(options) { }
